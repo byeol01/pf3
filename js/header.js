58d5bb5e287529c4efa77eb73e63bd4c5eb3bc32
byeol01 <hanbyeol3170@gmail.com>
2025-03-21 12:15:50 +0900

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    // $("header").css("background-color","red");
+    // $("header").css("top","20px");
+    $("header").css({"background-color":"red","top":"0px"});
+});
